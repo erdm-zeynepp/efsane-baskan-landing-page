9161833eb3302f27a727b54dfb4ba8340e0d1b5e
erdm-zeynepp <erdm.zeynepp@gmail.com>
2026-06-08 16:57:17 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,17 +4,17 @@ const result = document.getElementById("result");
 
 if(type === "Builder"){
 result.innerHTML =
-"You are The Builder 🏗️<br>You create powerhouse clubs through ambition and big signings.";
+"You are The Builder 🏗️<br>You create powerhouse clubs through vision, ambition and bold investments.";
 }
 
 if(type === "Scout"){
 result.innerHTML =
-"You are The Scout 🔍<br>You discover talent before everyone else.";
+"You are The Scout 🔍<br>You discover future stars before anyone else.";
 }
 
 if(type === "Tactician"){
 result.innerHTML =
-"You are The Tactician 🧠<br>You win through preparation and smart decisions.";
+"You are The Tactician 🧠<br>You win through preparation, analysis and smart decisions.";
 }
 
 }
