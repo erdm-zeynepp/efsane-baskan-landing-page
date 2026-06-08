353b118b7e2876d43d0d96c77193afb73b7576f8
erdm-zeynepp <erdm.zeynepp@gmail.com>
2026-06-08 16:30:13 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+function showResult(type){
 
+const result = document.getElementById("result");
+
+if(type === "Builder"){
+result.innerHTML =
+"You are The Builder 🏗️<br>You create powerhouse clubs through ambition and big signings.";
+}
+
+if(type === "Scout"){
+result.innerHTML =
+"You are The Scout 🔍<br>You discover talent before everyone else.";
+}
+
+if(type === "Tactician"){
+result.innerHTML =
+"You are The Tactician 🧠<br>You win through preparation and smart decisions.";
+}
+
+}
